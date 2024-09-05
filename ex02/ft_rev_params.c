e64e5f9b474fa896243542898b91870e1edf5238
Nicoli Martins Freitas <nicomart@c2r2p3.42sp.org.br>
2024-09-05 16:47:20 -0300

    c06

---
+++ ex02/ft_rev_params.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nicomart <nicomart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 18:41:38 by nicomart          #+#    #+#             */
+/*   Updated: 2024/09/05 15:47:16 by nicomart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = argc -1;
+	while (i > 0)
+	{
+		write(1, argv[i], ft_strlen(argv[i]));
+		write(1, "\n", 1);
+		i--;
+	}
+	return (0);
+}
